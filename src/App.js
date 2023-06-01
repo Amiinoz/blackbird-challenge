243e17bc7b77d88918fd5028daa77ccae000e2f2
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-06-01 13:49:48 -0600

    ⚡ change logo and home link

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"
+          alt="new logo"
+        />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://www.propelleraero.com/dirtmate/"
+          target="propelleraero website"
+          rel=" propelleraero website link"
         >
           Learn React
         </a>
