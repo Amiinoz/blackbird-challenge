26d3cea02ca75d8a3a9cedd5c4587bbbb86439d7
Amiinoz <amiinoz@Mohameds-MacBook-Air.local>
2023-06-06 11:44:48 -0600

    handing off command.sh file

---
+++ command.sh
@@ -0,0 +1,35 @@
+
+    1.  npx create-react-app blackbird-challenge
+
+    2.  cd blackbird-challenge
+        create a new repo called blackbird-challege
+        git remote add origin https://github.com/Amiinoz/blackbird-challenge.git
+        git branch -M main
+        git push -u origin main
+
+    3.  git checkout -b  update_logo
+
+    4.  // In app.js replace the logo
+				<img
+          src="https://www.propelleraero.com/wp-content/uploads/2021/05/Vector.svg"
+          alt="new logo"
+        />
+
+    5. // In app.js replace the home link
+				<a
+          className="App-link"
+          href="https://www.propelleraero.com/dirtmate/"
+          target="propelleraero website"
+          rel=" propelleraero website link"
+        >
+
+    6.  git add .
+        git commit -m"⚡ change logo and home link"
+        git push
+
+    8.  git checkout -b  master
+
+    9.  git checkout master
+        git merge master
+
+Link to repo: https://github.com/Amiinoz/blackbird-challenge/tree/main
